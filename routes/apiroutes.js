f2cff51e43c222fe8bef76f0df269c2f1456daf6
Nathan Hill <pjsstp@gmail.com>
2019-10-09 15:28:55 -0400

    finished authentication scripts for passport using mongoose orm

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,16 +1,15 @@
 const middleware = require("../middleware");
-const API = require("../utils")
 const passport = middleware.passport;
-// const isAuthenticated = middleware.isAuthenticated;
+const User = require("../models/User")
 
 module.exports = app => {
 
   // create a GET route
   app.get('/express_backend', (req, res) => {
-    var answer = API.getExpressBackend()
-    res.send(answer);
+    res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
   });
 
+  // Endpoint to login
   app.post('/login',
     passport.authenticate('local'), (req, res) => {
       res.send(req.user);
@@ -19,32 +18,39 @@ module.exports = app => {
 
   app.get('/logout', (req, res) => {
     req.logout();
-    res.send(null)
+    res.send({ result: "success" })
   });
 
   // Endpoint to get current user
-  app.get('/getuser', (req, res) => {
+  app.get("/getuser", (req,res) => {
+    if (!req.user) res.send({ result: "no user" })
     res.send(req.user);
   });
 
-  // Register User
+// Register User
   app.post('/register', (req, res) => {
+    var password = req.body.password;
+    var password2 = req.body.password2;
 
-    var newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: req.body.password,
-      income: req.body.income
-    });
+    if (password == password2){
+      var newUser = new User({
+        name: req.body.name,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        income: req.body.income,
+        username: req.body.username,
+        password: req.body.password
+      });
 
-    User.createUser(newUser, (err, user) => {
-      if(err) throw err;
-      res.send(user).end()
-    });
+      User.createUser(newUser, (err, user) => {
+        if(err) console.log(err);
+        res.send(user).end();
+      });
+    } else{
+      res.status(500).send("{errors: \"Passwords don't match\"}").end()
+    }
   });
-
 }
   
 
