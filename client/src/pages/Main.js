c17a38efbd01ef1132fcaf33d395063cbce3c3fd
Casey Carroll <caseydean8@gmail.com>
2019-10-20 08:18:35 -0400

    Subscription page displays to  bottom of window

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -4,17 +4,15 @@ import SubForm from "../components/SubForm"
 import "./SignIn.css";
 
 function Main (props) {
-    return (
-    <div className="App">
-			<div className="input-fields">
+	return (
+		<div className="input-fields">
 			<SubList
 			subscriptions={props.subscriptions}
 			removeSub={props.removeSub} />
 			<SubForm
 			addSub={props.addSub}/>
-			</div>
-    </div>
-    )
+		</div>
+	)
 }
 
 export default Main
