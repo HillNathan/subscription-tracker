49375e689c49900ba38326e1e62f6475932777c3
Casey Carroll <caseydean8@gmail.com>
2019-10-23 11:29:15 -0400

    logo appears on main page.

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
+import SubLogo from "./yellowSub.png";
 
 function Logo() {
   return (
 		<div className="subLogo">
-
+			<img src={SubLogo} alt="sublogo"></img>
 		</div>
 	)
 }
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,14 +3,18 @@ import SubList from "../components/SubList";
 import SubForm from "../components/SubForm";
 import PaddingDiv from "../components/PaddingDiv"
 import "../pages/Subscription/style.css";
+import Logo from "../components/Logo"
 
 function Main(props) {
   return (
     <div className="App row">
       <div className="container">
         <PaddingDiv height={30} />
+        <div className="col-sm">
+          <Logo />
+        </div>
         <div className="row justify-content-center">
-          <div className="col-10 subscription form-holder">
+          <div className="col-lg-8 subscription form-holder">
             <SubList
               subscriptions={props.subscriptions}
               removeSub={props.removeSub}
