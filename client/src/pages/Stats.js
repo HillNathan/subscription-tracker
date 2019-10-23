fe54a7455d2862e7a21400b65357ce0bc11c7cbf
Nico Barbarese <nicobarbarese@gmail.com>
2019-10-22 20:05:29 -0400

    total expenditure and monthly percentage  of income stats

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -95,12 +95,12 @@ function monthlySum (subArr) {
         total += array[i].cost
     }
     console.log(total)
-    return total
+    return Number(total.toFixed(2))
 }
 
 function incomeRatio (subArr, income) {
     let total = monthlySum(subArr)
-    let ratio = (total/income)*100+"%"
+    let ratio = ((total/income)*100).toFixed(2)+"%"
     return ratio
 }
 
@@ -113,6 +113,10 @@ function Stats (props) {
                 <Chart2 
                 data = {makeDataMonthly(props.subscriptions)} />
                 {/* <h3>Monthly Subscription Breakdown</h3> */}
+            </div>
+            <div className= "col text-align-center text-white">
+                <p>You are spending a total of ${monthlySum(props.subscriptions)} on subscriptions.</p>
+                <p>This accounts for {incomeRatio(props.subscriptions, props.income)} of your monthly budget.</p>
             </div>
         </div>
 
