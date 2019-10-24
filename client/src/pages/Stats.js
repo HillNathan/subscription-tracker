0e52e2fc3658a2c06607092e3ca3de24188f574b
Nathan Hill <pjsstp@gmail.com>
2019-10-23 21:36:58 -0400

    updated stats page to respond to checking and unchecking of boxes

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -6,10 +6,38 @@ function getMonthlyTotal(subArray) {
     let sum = 0;
     if (subArray.length > 0) {
         for (let i = 0; i < subArray.length; i++) {
+            if (subArray[i].active) {
+                switch (subArray[i].frequency){
+                    case "Monthly":
+                    sum += subArray[i].cost;
+                    break;
+                    case "Weekly":
+                        sum += (subArray[i].cost * 52) / 12;
+                        break;
+                    case "Yearly":
+                        sum += subArray[i].cost / 12;
+                        break;
+                    case "Daily":
+                        sum += (subArray[i].cost * 365) /12;
+                        break;
+                    default:
+                        sum += subArray[i].cost;
+                        break;
+                } 
+            }   
+        }
+    }
+    return Number(sum.toFixed(2));
+}
+
+function initialMonthlyTotal(subArray) {
+    let sum = 0;
+    if (subArray.length > 0) {
+        for (let i = 0; i < subArray.length; i++) {
             switch (subArray[i].frequency){
                 case "Monthly":
-                   sum += subArray[i].cost;
-                   break;
+                sum += subArray[i].cost;
+                break;
                 case "Weekly":
                     sum += (subArray[i].cost * 52) / 12;
                     break;
@@ -22,10 +50,9 @@ function getMonthlyTotal(subArray) {
                 default:
                     sum += subArray[i].cost;
                     break;
-            }    
-        }
+            } 
+        }   
     }
-    console.log("Monthly Sum from fn" + sum)
     return Number(sum.toFixed(2));
 }
 
@@ -33,10 +60,39 @@ function getYearlyTotal(subArray) {
     let sum = 0;
     if (subArray.length > 0) {
         for (let i = 0; i < subArray.length; i++) {
+            if(subArray[i].active) {
+                switch (subArray[i].frequency){
+                    case "Yearly":
+                    sum += subArray[i].cost;
+                    break;
+                    case "Weekly":
+                        sum += (subArray[i].cost * 52);
+                        break;
+                    case "Monthly":
+                        sum += subArray[i].cost * 12;
+                        break;
+                    case "Daily":
+                        sum += (subArray[i].cost * 365);
+                        break;
+                    default:
+                        sum += subArray[i].cost;
+                        break;
+                }  
+            }  
+        }
+    }
+    console.log("Yearly Sum from fn" + sum)
+    return Number(sum.toFixed(2));
+}
+
+function initialYearlyTotal(subArray) {
+    let sum = 0;
+    if (subArray.length > 0) {
+        for (let i = 0; i < subArray.length; i++) {
             switch (subArray[i].frequency){
                 case "Yearly":
-                   sum += subArray[i].cost;
-                   break;
+                sum += subArray[i].cost;
+                break;
                 case "Weekly":
                     sum += (subArray[i].cost * 52);
                     break;
@@ -49,12 +105,13 @@ function getYearlyTotal(subArray) {
                 default:
                     sum += subArray[i].cost;
                     break;
-            }    
+            }  
         }
     }
     console.log("Yearly Sum from fn" + sum)
     return Number(sum.toFixed(2));
 }
+
 
 function makeDataMonthly(subArr) {
     let result = [];
@@ -107,6 +164,13 @@ function incomeRatio (subArr, income) {
     return ratio
 }
 
+function initialIncomeRatio (subArr, income) {
+    let total = initialMonthlyTotal(subArr)
+    let ratio = ((total/income)*100).toFixed(2) + "%"
+    return ratio
+}
+
+
 class Stats extends Component {
 
     state = {
@@ -116,17 +180,46 @@ class Stats extends Component {
         statSubscriptions: [],
     }
 
-    componentWillReceiveProps(incomingProps) {
-        this.setState({ yearlyTotal: getYearlyTotal(incomingProps.subscriptions) })
-        this.setState({ monthlyTotal: getMonthlyTotal(incomingProps.subscriptions) })
-        this.setState({ ratio: incomeRatio(incomingProps.subscriptions, incomingProps.income) })
+    handleCheckboxChange(index, event) {
+        let tempArr = this.state.statSubscriptions
+        if (event) tempArr[index].active = false
+        else tempArr[index].active = true
+        this.setState({ statSubscriptions: tempArr })
+        this.setState({ monthlyTotal: getMonthlyTotal(this.state.statSubscriptions) })
+        this.setState({ ratio: incomeRatio(this.state.statSubscriptions, this.props.income)})
+    }
+
+    componentWillReceiveProps(incomingProps) {        
         this.setState({ statSubscriptions: setUpSubs(incomingProps.subscriptions) })
+        this.setState({ yearlyTotal: initialYearlyTotal(incomingProps.subscriptions) })
+        this.setState({ monthlyTotal: initialMonthlyTotal(incomingProps.subscriptions) })
+        this.setState({ ratio: initialIncomeRatio(incomingProps.subscriptions, incomingProps.income) })
     }
 
 render() {
     return (
     <div className="container justify-content-center">
         <div className="row">
+            <div className = "col-3">
+                <h3>Subscriptions</h3>
+                <br />
+                {this.state.statSubscriptions.map((elem, i) => {
+                    return (
+                        <div className = "form-check" >
+                            <input className = "form-check-input" 
+                                    type = "checkbox"
+                                    checked = {elem.active}
+                                    id = {elem.name}
+                                    name = {elem.name}
+                                    onClick = {() => this.handleCheckboxChange(i, elem.active) } />
+                            <label className = "form-check-label" 
+                                    htmlFor = {elem.name} >
+                            {elem.name}
+                            </label>
+                        </div>
+                    )
+                })}
+            </div>
             <div className = "col text-align-center">
                 <Chart2 
                 data = {makeDataMonthly(this.state.statSubscriptions)} />
