d4f3f30e1d5c42be152c2f9b01afcef684f1b1b2
Nathan Hill <pjsstp@gmail.com>
2019-10-26 13:29:37 -0400

    server and config files updated for heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const app = require("./config/express-config");
-const mongoose = require("mongoose")
-const PORT = process.env.PORT || 5000
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
 
 // connect to MongoDB
-mongoose.connect("mongodb://localhost/submarine", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // console.log that your server is up and running
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
