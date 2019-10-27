2a8908ac27aba8ad54b363387aa486a4b5525c28
Casey Carroll <caseydean8@gmail.com>
2019-10-27 12:13:39 -0400

    cost and fequency same size on mobile

--- client/src/components/SubForm/index.js
+++ client/src/components/SubForm/index.js
@@ -1,51 +1,65 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
-function cb () {
-    console.log("Callback hit")
-    document.getElementById("name-input").value = "";
-    document.getElementById("cost-input").value = "";
-    document.getElementById("frequency-input").value = ""
+function cb() {
+  console.log("Callback hit");
+  document.getElementById("name-input").value = "";
+  document.getElementById("cost-input").value = "";
+  document.getElementById("frequency-input").value = "";
 }
 
 function SubForm(props) {
-    return (
-        <div className = "row">
-            <div className="col">
-                <form id="sub-form">
-                    <div className="form-group">
-                        <label htmlFor="name">Subscription Name</label>
-                        <input className="form-control form-control-lg" id="name-input"></input>
-                    </div>
-                    <div className="form-group">
-                        <div className = "row" >
-                            <div className = "col-6">
-                                <label htmlFor="cost">Cost (USD):</label>
-                                <input className="form-control form-control-lg" id="cost-input"></input>
-                             </div>
-                            <div className = "col">
-                                <label htmlFor="frequency">Frequency</label>
-                                <select className="form-control form-control-lg" id="frequency-input">
-                                    <option>Daily</option>
-                                    <option>Weekly</option>
-                                    <option>Monthly</option>
-                                    <option>Yearly</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <button onClick={(event) => props.addSub(event, cb, 
-                        { 
-                            name: document.getElementById("name-input").value,
-                            cost: document.getElementById("cost-input").value,
-                            frequency: document.getElementById("frequency-input").value 
-                        })}
-                        className="Formfield__Button" 
-                        id="form-submit">Submit</button>
-                </form>
+  return (
+    <div className="row">
+      <div className="col-sm">
+        <form id="sub-form">
+          <div className="form-group">
+            <label htmlFor="name">Subscription Name</label>
+            <input
+              className="form-control form-control-lg"
+              id="name-input"
+            ></input>
+          </div>
+          <div className="form-group">
+            <div className="row">
+              <div className="col-sm">
+                <label htmlFor="cost">Cost (USD):</label>
+                <input
+                  className="form-control form-control-lg"
+                  id="cost-input"
+                ></input>
+              </div>
+              <div className="col-sm">
+                <label htmlFor="frequency">Frequency</label>
+                <select
+                  className="form-control form-control-lg"
+                  id="frequency-input"
+                >
+                  <option>Daily</option>
+                  <option>Weekly</option>
+                  <option>Monthly</option>
+                  <option>Yearly</option>
+                </select>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+          <button
+            onClick={event =>
+              props.addSub(event, cb, {
+                name: document.getElementById("name-input").value,
+                cost: document.getElementById("cost-input").value,
+                frequency: document.getElementById("frequency-input").value
+              })
+            }
+            className="Formfield__Button"
+            id="form-submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default SubForm
+export default SubForm;
